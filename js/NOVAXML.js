19506018ddf2775f7ed912bddb4634ce684137b6
Kirens <eriknygren97@gmail.com>
2014-07-07 14:38:35 +0200

    Structure build initiated
    Main structure build initiated. Organizing still needed

--- js/NOVAXML.js
+++ js/NOVAXML.js
@@ -1,41 +1,115 @@
-var NOVA = NOVA || {};
-
-window.onload = function() {
-    if (typeof PDFJS === 'undefined') {
-        alert('Built version of pdf.js is not found\nPlease run `node make generic`');
-        return;
-    }
-
-    var scale = 1.5; //Set this to whatever you want. This is basically the "zoom" factor for the PDF.
-    //PDFJS.workerSrc = '../../build/generic/build/pdf.worker.js';
-
-    PDFJS.getDocument("Schedule.pdf").then(function(pdf) {
-        pdf.getPage(1).then(function(page) {
-
-            var scale = 1.5;
-            var viewport = page.getViewport(scale);
-
-            var container = document.getElementById("pdfContainer");
+var NOVA = NOVA || {
+    create: function(){
+    },
+    start: function(){
+    },
+    Schedule: function(){
+        
+        //Change these to prototype
+        this.toCSV = function(){
+        };
+        this.toXML = function(){
+        };
+        this.toICAL = function(){
+        };
+    },
+    week: function(nr, parent){
+        
+        var days = {};
+        
+        var get = function(loc, scale, completeFn, textAnalysisFn){
+            if (typeof PDFJS === 'undefined') {
+                alert('Built version of pdf.js is not found\nPlease run `node make generic`');
+                return;
+            }
             
-            var canvas = document.createElement("canvas");
-            var context = canvas.getContext('2d');
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-
-            container.appendChild(canvas);
-
-            page.getTextContent().then(function(textContent) {
-                
-                var renderContext = {
-                    canvasContext: context,
-                    viewport: viewport
-                };
-                page.render(renderContext);
-
-
+            PDFJS.getDocument(loc).then(function(pdf) {
+                pdf.getPage(1).then(function(page) {
+                    
+                    var viewport;
+                    //Make folowing a function?
+                    if(scale.width || scale.height){
+                        //if scale is sent as an object {width: int, height: int, prio: 'max/min'}
+                        viewport = page.getViewport(1);
+                        
+                        if(!scale.height){
+                            //if only width is sent
+                            viewport = page.getViewport(scale.width/viewport.width);
+                        }else if(!scale.width){
+                            //if only height is sent
+                            viewport = page.getViewport(scale.height/viewport.height);
+                        }else{
+                            //if both are defined chose priority of scale; min/max value. If scale.prio not defined, set it to min
+                            //min/max works as as css-background css contain/cover
+                            var widhtChange = scale.width/viewport.width,
+                                heightChange = scale.height/viewport.height;
+                            
+                            if(scale.prio == 'max'){
+                                viewport = page.getViewport(Math.max(widhtChange,heightChange));
+                            }else{
+                                viewport = page.getViewport(Math.min(widhtChange,heightChange));
+                            }
+                        }
+                        
+                    }else if(!isNaN(parseFloat(scale))){
+                        //if scale is scale value
+                        viewport = page.getViewport(scale);
+                    }else{
+                        //if scale is not understood
+                        viewport = page.getViewport(1);
+                    }
+                    
+                    var canvas = document.querySelector('canvas');
+                    var context = canvas.getContext('2d');
+                    
+                    canvas.height = viewport.height;
+                    canvas.width = viewport.width;
+                    canvas.parentElement.style.width = viewport.width + 'px';
+                    
+                    
+                    page.getTextContent().then(function(textContent) {
+                                                
+                        var renderContext = {
+                            canvasContext: context,
+                            viewport: viewport
+                        };
+                        
+                        var render;
+                        
+                        if(typeof textAnalysisFn === 'function')
+                            render = textAnalysisFn(renderContext);
+                        
+                        if(render !== false){
+                            var renderObj = page.render(renderContext);
+                            if(typeof completeFn === 'function')
+                                renderObj.then(completeFn);
+                        }
+                    });
+                });
             });
-
-
-        });
-    });
+        };
+    }
 };
+
+window.onload = createStuff;
+
+function createCanvasImage(canvas){
+    var el = document.createElement('div');
+    el.style.backgroundImage = 'url(' + canvas.toDataURL('data:image/png') + ')';
+    el.style.width = canvas.width + 'px';
+    el.style.height = canvas.height + 'px';
+    canvas.parentElement.appendChild(el);
+    //canvas.parentElement.removeChild(canvas);
+}
+
+function createStuff(){
+    var container = document.getElementById('pdfContainer');
+
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+
+    container.appendChild(canvas);
+    
+    var tmp = new NOVA.Week();
+    tmp.get('Schedule.pdf', {width:Math.min(document.body.getBoundingClientRect().width-20, 900),height:10,prio:'max'});
+}
